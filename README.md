7c4c3393d72deb2321285a46c01d63c73dece02c
Sawood Alam <ibnesayeed@gmail.com>
2018-09-23 15:51:33 -0400

    Add step6 branch

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Link Extractor
+# Link Extractor: Step 6
 
-A Docker tutorial using a link extraction application example.
+A web application to extract links and anchor texts from a given web page and analyze link statistics.
 
-The code of various steps in the `master` branch are organized in sub-directories, but for demo purposes those individual directories are moved to the root of the repo and each step is made a sub-branch of the `demo` branch.
+## Changes from the previous step
+
+* The API service written in Python is replaced with a similar Ruby implementation
+* The `API_ENDPOINT` is updated to point to the new Ruby API service
+* Newly added link extraction log is persisted using volumes
+
+## Try it out
+
+```
+$ docker-compose up --build
+```
+
+Open http://localhost/?url=http%3A%2F%2Fodu.edu%2F in a web browser.
+
+Press `Ctrl + C` to terminate the service.
---
+++ api/linkextractor.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "sinatra"
+require "open-uri"
+require "uri"
+require "nokogiri"
+require "json"
+require "redis"
+
+set :protection, :except=>:path_traversal
+
+redis = Redis.new(host: "redis", port: 6379)
+
+Dir.mkdir("logs") unless Dir.exist?("logs")
+cache_log = File.new("logs/extraction.log", "a")
+
+get "/" do
+  "Usage: http://<hostname>[:<prt>]/api/<url>"
+end
+
+get "/api/*" do
+  url = [params['splat'].first, request.query_string].reject(&:empty?).join("?")
+  cache_status = "HIT"
+  jsonlinks = redis.get(url)
+  if jsonlinks.nil?
+    cache_status = "MISS"
+    jsonlinks = JSON.pretty_generate(extract_links(url))
+    redis.set(url, jsonlinks)
+  end
+
+  cache_log.puts "#{Time.now.to_i}\t#{cache_status}\t#{url}"
+
+  status 200
+  headers "content-type" => "application/json"
+  body jsonlinks
+end
+
+def extract_links(url)
+  links = []
+  doc = Nokogiri::HTML(open(url))
+  doc.css("a").each do |link|
+    text = link.text.strip.split.join(" ")
+    begin
+      links.push({
+        text: text.empty? ? "[IMG]" : text,
+        href: URI.join(url, link["href"])
+      })
+    rescue
+    end
+  end
+  links
+end
