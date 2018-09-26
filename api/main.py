3dbb7ebfa4a6eb61dc10aff09c11fa58300925cb
Sawood Alam <ibnesayeed@gmail.com>
2018-09-25 21:55:15 -0400

    Synchronize branch step5

--- api/main.py
+++ api/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
+import os
+import json
+import redis
 from flask import Flask
 from flask import request
-from redis import Redis
-import json
 from linkextractor import extract_links
 
 app = Flask(__name__)
-redis = Redis(host="redis", port=6379)
+redis_conn = redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
 
 @app.route("/")
 def index():
@@ -19,11 +20,11 @@ def api(url):
     if qs != "":
         url += "?" + qs
 
-    jsonlinks = redis.get(url)
+    jsonlinks = redis_conn.get(url)
     if not jsonlinks:
         links = extract_links(url)
         jsonlinks = json.dumps(links, indent=2)
-        redis.set(url, jsonlinks)
+        redis_conn.set(url, jsonlinks)
 
     response = app.response_class(
         status=200,
